ccf3db7a2d4aecd79da9138be7e5a750a265ef30
Tanner Norris <jiujitsucarrot@Tanners-MacBook-Air.local>
2023-06-28 22:07:44 -0700

    Made text appear in box when button is clicked.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Button Activated");
+
+  return "Password will appear here."
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
